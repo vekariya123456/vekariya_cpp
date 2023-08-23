a4efb81d995e02696a6875ab55293c9814daa0bf
Vijay Vekariya <vijayvekariya007@gmail.com>
2023-08-23 08:08:24 +0530

    change

---
+++ triangal.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t,b,h;
+
+    cout<<"enter b value=";
+    cin>>b;
+
+    cout<<"Enter h value=";
+    cin>>h;
+
+    t=(b*h)/2;
+
+    cout<<"area of Triangal is:"<<t;
+}
