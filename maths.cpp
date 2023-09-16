7c7bf95b4aee84511583b8468f31b95f0224b7e5
Vijay Vekariya <vijayvekariya007@gmail.com>
2023-09-16 09:53:04 +0530

    16 sep

---
+++ maths.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Ceil number is:" << ceil(3.5) << endl;
+    cout <<"Ceil number is:" << ceil(3.1) << endl;
+    cout <<"Ceil number is:" << ceil(3.9) << endl;
+    cout <<"Floor number is:" << floor(5.9) << endl;
+    cout <<"Floor number is:" << floor(5.2) << endl;
+    cout <<"Floor number is:" << floor(5.9) << endl;
+    cout <<"Round number is:" << round(8.9) << endl;
+    cout <<"Round number is:" << round(8.5) << endl;
+    cout <<"Round number is:" << round(8.2) << endl;
+    cout <<"SQRT number is:"  << sqrt(16) << endl;
+    cout <<"SQRT number is:"  << sqrt(4) << endl;
+    cout <<"Power number is:" << pow(4,2) << endl;
+    cout <<"Power number is:" << pow(8,3) << endl;
+    cout <<"ABS number is:"   << abs(-8) << endl;
+    cout <<"ABS number is:"   << abs(-15) << endl;
+    cout <<"ABS number is:"   << abs(89) << endl;
+        
+}
