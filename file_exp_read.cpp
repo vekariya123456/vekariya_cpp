6c0ee6e0f0bf0858465d78945d6cf25434a96e88
Vijay Vekariya <ketsgajera31@gmail.com>
2023-09-29 10:02:27 +0530

    29 sep

--- file_exp_read.cpp
+++ file_exp_read.cpp
@@ -1,3 +1,9 @@
+//filemanagment
+//ofstream: stream class to write on file
+//ifstream: srtream class to read from file
+//ffstream: stream class to both read and write from/to file
+
+
 #include<iostream>
 #include<fstream>
 
